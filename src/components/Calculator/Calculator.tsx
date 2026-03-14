85cf7e6ba417e1fc21cdbdf777aea050983d6e50
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2026-03-14 12:20:15 +0530

    Remove unused imports

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './calculator.scss';
-import { MdDeleteOutline } from 'react-icons/md';
 
 export default function Calculator() {
   const [display, setDisplay] = useState('0');
